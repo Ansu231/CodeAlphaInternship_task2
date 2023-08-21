0b983855d6458bf45bd3da8e1d2b63797924bf90
Ankita karki <87992428+Ansu231@users.noreply.github.com>
2023-08-22 00:31:04 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function openPopup(popupId) {
-    var popup = document.getElementById(popupId);
-    popup.classList.add('active');
-}
-
-function closePopup(popupId) {
-    var popup = document.getElementById(popupId);
-    popup.classList.remove('active');
-}
